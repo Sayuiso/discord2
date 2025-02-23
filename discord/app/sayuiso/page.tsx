5f71c10ecdc50b7d3fe17ecb72fc0f0f66b8fbd6
sayuiso <sayuiso02@gmail.com>
2025-02-23 12:44:06 +0000

    時間が中央ぞろえにならないよ

--- discord/app/sayuiso/page.tsx
+++ discord/app/sayuiso/page.tsx
@@ -1,31 +1,54 @@
+"use client";
 import "./layout.css";
 
+import Clock from "@/components/ui/clock";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [time, setTime] = useState<Date | null>(null);
+
+  useEffect(() => {
+    setTime(new Date());
+
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (time === null) return null;
+
+  console.log(time);
+
   return (
-    <div className="chat-container">
-      <div className="tabs">
-        <button className="tab-button">すべて表示</button>
+    <>
+        <Clock time={time} />
+      <div className="chat-container">
+        <div className="tabs">
+          <button className="tab-button">すべて表示</button>
+        </div>
+        <div className="chat-display">
+          <div className="system-message">緊急警報発令。</div>
+          <div className="system-message">大規模作戦を開始します。</div>
+          <div className="chat-message">気が遠くなりそう</div>
+        </div>
+        <div className="chat-input">
+          <input type="text" placeholder="周囲に発言" />
+          <button className="toggle-dropdown">▽</button>
+        </div>
+        <div className="buttons">
+          <button className="icon-button">🗨️</button>
+          <button className="icon-button">📷</button>
+          <button className="icon-button">👤</button>
+          <button className="icon-button">👥</button>
+          <button className="icon-button">📧</button>
+          <button className="icon-button">➡️</button>
+          <button className="send-button">発言</button>
+          <button className="icon-button">⚙️</button>
+          <button className="icon-button">🗂️</button>
+        </div>
       </div>
-      <div className="chat-display">
-        <div className="system-message">緊急警報発令。</div>
-        <div className="system-message">大規模作戦を開始します。</div>
-        <div className="chat-message">気が遠くなりそう</div>
-      </div>
-      <div className="chat-input">
-        <input type="text" placeholder="周囲に発言" />
-        <button className="toggle-dropdown">▽</button>
-      </div>
-      <div className="buttons">
-        <button className="icon-button">🗨️</button>
-        <button className="icon-button">📷</button>
-        <button className="icon-button">👤</button>
-        <button className="icon-button">👥</button>
-        <button className="icon-button">📧</button>
-        <button className="icon-button">➡️</button>
-        <button className="send-button">発言</button>
-        <button className="icon-button">⚙️</button>
-        <button className="icon-button">🗂️</button>
-      </div>
-    </div>
+    </>
   );
 }
